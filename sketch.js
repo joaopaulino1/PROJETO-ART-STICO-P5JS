cbcee92dc08d66dc41deccacf37ab643905345fd
Paulino <169078112+joaopaulino1@users.noreply.github.com>
2024-05-22 09:37:10 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 600);
+  background("black")
+}
+function draw() {
+  stroke("blue")
+  fill("yellow");
+  if(mouseIsPressed){
+    triangle(mouseX, mouseY, 20, 35);
+}
+}
